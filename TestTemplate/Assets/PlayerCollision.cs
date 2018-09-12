ca5692707699733e55a388a547e0ef2166330453
danbokhari98 <danbokhari98@gmail.com>
2018-09-12 17:51:01 -0400

    Test Template for Github

---
+++ TestTemplate/Assets/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement Movement;
+
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            Movement.enabled = false;
+        }
+        
+    }
+
+}
